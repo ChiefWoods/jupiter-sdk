df06a62165f4103ef6246cb2ff3874191e13d6e4
Chii Yuen <chii.yuen@hotmail.com>
2026-07-26 03:14:05 +0800

    chore: add oxlint

---
+++ oxlint.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "oxlint";
+
+export default defineConfig({
+  plugins: ["typescript", "unicorn", "oxc", "import", "promise", "vitest"],
+  categories: {
+    correctness: "error",
+  },
+  rules: {
+    "no-unused-vars": "warn",
+  },
+  env: {
+    builtin: true,
+  },
+});
